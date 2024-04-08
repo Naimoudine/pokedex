75b10df557e369e72072954104152768f0f80644
Naïm <naimoudinesaidi@gmail.com>
2024-04-08 16:44:17 +0200

    add pokemon data & changed code

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,21 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
+    let pokemon = pokemonList[0];
+
     return(
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
+            {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name}/> : "???"}
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     )
 }
