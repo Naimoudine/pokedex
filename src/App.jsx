f62bc0f4da10a1f5d7543aab087bd48da54483ee
Naïm <naimoudinesaidi@gmail.com>
2024-04-08 18:19:59 +0200

    shared state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css'
 import PokemonCard from './components/PokemonCard';
+import NavBar from './components/Navbar';
 
 const pokemonList = [
   {
@@ -27,28 +28,16 @@ const pokemonList = [
       name: "mew",
     },
   ];
-  
+
 function App() {
   const [pokeIndex, setPokeIndex] = useState(0);
-
-  const handlePrevious = () => {
-    setPokeIndex(pokeIndex - 1);
-  }
-
-  const handleNext = () => {
-    setPokeIndex(pokeIndex + 1);
-  }
 
   return (
     <>
       <div>
         <PokemonCard pokemon={pokemonList[pokeIndex]}/>
-        <div>
-          {pokeIndex > 0 ? <button onClick={handlePrevious}>précedent</button> : ""}
-
-          {pokeIndex < pokemonList.length - 1 ? <button onClick={handleNext}>suivant</button> : ""}
-        </div>
       </div>
+      <NavBar pokeIndex={pokeIndex} setPokeIndex={setPokeIndex} pokemonList={pokemonList} />
     </>
   )
 }
